0c7ec070cb58eec93b55ff9822f49d6ef042d8c4
Celes <209721999+Cell3s@users.noreply.github.com>
2025-09-26 13:05:23 -0400

    Updating Rocket Countdown

--- Array&Loops/RocketCountdown/app.js
+++ Array&Loops/RocketCountdown/app.js
@@ -1,5 +1,43 @@
 const output = document.querySelector(".output");
+const button = document.querySelector(".btn");
+const clearButton = document.querySelector(".clearBtn");
 output.textContent = "";
+
+function countDown (){
+for (let i = 10; i>=0; i--){
+    const para = document.createElement("p")
+    let count = `${i} seconds until launch`;
+    output.textContent += `${count}\n`;
+    output.appendChild(para)
+}
+    output.textContent += '\nWe have life off!\n\n';
+}
+
+
+
+// function countDown (){
+// let i = 10;
+
+// while (i >= 0) {
+//   const para = document.createElement("p");
+//   if (i === 10) {
+//     para.textContent = `Countdown ${i}`;
+//   } else if (i === 0) {
+//     para.textContent = "Blast off!";
+//   } else {
+//     para.textContent = i;
+//   }
+//   output.appendChild(para);
+
+//   i--;
+
+// }
+
+// }
+
+
+button.addEventListener("click", countDown);
+clearButton.addEventListener("click", () => (output.textContent=""));
 
 // let i = 10;
 
